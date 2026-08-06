d0de7f5ce26d627178e0c038572dcd63aeb97c8f
lp_tower <abovethetower@outlook.com>
2026-08-06 10:20:58 +0800

    lab4

--- lab04/ex1.s
+++ lab04/ex1.s
@@ -50,6 +50,8 @@ next_test:
 pow:
     # BEGIN PROLOGUE
     # FIXME: Need to save the callee saved register(s)
+    addi sp, sp, -4
+    sw s0, 0(sp)
     # END PROLOGUE
     li s0, 1
 pow_loop:
@@ -61,6 +63,8 @@ pow_end:
     mv a0, s0
     # BEGIN EPILOGUE
     # FIXME: Need to restore the callee saved register(s)
+    lw s0, 0(sp)
+    addi sp, sp, 4
     # END EPILOGUE
     jr ra
 
@@ -73,8 +77,10 @@ pow_end:
 inc_arr:
     # BEGIN PROLOGUE
     # FIXME: What other registers need to be saved?
-    addi sp, sp, -4
+    addi sp, sp, -12
     sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
     # END PROLOGUE
     mv s0, a0 # Copy start of array to saved register
     mv s1, a1 # Copy length of array to saved register
@@ -86,8 +92,12 @@ inc_arr_loop:
     # Prepare to call helper_fn
     #
     # FIXME: Add code to preserve the value in t0 before we call helper_fn
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
     # Also ask yourself this: why don't we need to preserve t1?
-    #
+    # t1 is caller-saved.
     jal ra helper_fn
     # FIXME: Restore t0
     # Finished call for helper_fn
@@ -112,6 +122,8 @@ inc_arr_end:
 helper_fn:
     # BEGIN PROLOGUE
     # FIXME: YOUR CODE HERE
+    addi sp, sp, -4
+    sw s0, 0(sp)
     # END PROLOGUE
     lw t1, 0(a0)
     addi s0, t1, 1
@@ -119,6 +131,8 @@ helper_fn:
     # BEGIN EPILOGUE
     # FIXME: YOUR CODE HERE
     # END EPILOGUE
+    lw s0, 0(sp)
+    addi sp, sp, 4
     jr ra
 
 # YOU CAN IGNORE EVERYTHING BELOW THIS COMMENT
